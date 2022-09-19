30e731d0c2ad783a31ef532a40aa16dabde24df6
shweta dhande <shweta dhande@LAPTOP-SJB79FIP>
2022-09-19 22:39:36 +0530

    First commit

---
+++ Day 3_JPA Queries/src/com/cg/service/BookService.java
@@ -0,0 +1,18 @@
+package com.cg.service;
+
+import java.util.List;
+
+import com.cg.dao.BookDao;
+import com.cg.entities.Book;
+
+public interface BookService 
+{
+	public abstract Book getBookById(int Id);
+	public abstract List<Book>getBookByTitle(String title);
+	public abstract List<Book> getBookByAuthor(String author);
+	public abstract List<Book> getBookInPriceRange(double low,double high);
+	public abstract List<Book>getAllBooks();
+	public abstract Long getBookCount();
+	
+
+}
